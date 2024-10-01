3167cc6018399bdac4ee34b794b397eb13ed14e9
_ <@gmail.com>
2024-10-01 19:29:21 -0400

    FIXED SOME STUFF .ENV FILE

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDEcKskXdLH5ZVOqkfSgSrm5vypB1yM84",
-  authDomain: "inventory-management-2adb9.firebaseapp.com",
-  projectId: "inventory-management-2adb9",
-  storageBucket: "inventory-management-2adb9.appspot.com",
-  messagingSenderId: "139719471960",
-  appId: "1:139719471960:web:9067822f587145de9a6b2d",
-  measurementId: "G-Y9QEXWBPF4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
